6560b0ab298bd319e68c730d6e7ae36b530cfa4b
WagnerAndy <wagner.andrew.james@gmail.com>
2018-11-23 20:46:34 -0700

    Everything but alignment

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -46,20 +46,26 @@ buttons ();
     }).then(function(response) {
         console.log (response);
 
-        var gifRow = $("<div class='row m-1 border border-primary'>");
-        var gifDiv = $("<div class='col border border-primary clearfix'>");
+        var gifRow = $("<div class='row m-2 p-1 border border-primary'>");
+        var gifDiv = $("<div class='col border border-primary'>");
+        var gifFig = $("<figure class='figure m-1'>");
+
         for (var i = 0; i < response.data.length; i++) {
 
+            var image = $("<img class='figure-img .img-fluid rounded gif' 'style: width=100%'>")
+            var imageURL = response.data[i].images.fixed_height_small_still.url;
+            var imageCaption = $("<figcaption class='figure-caption'>" + "Rating: " + (response.data[i].rating).toUpperCase () + "</figcaption>");
 
-
-            var authorGif = $("<img class='m-1'>");
-            var gifURL = response.data[i].images.fixed_height_small.url;
-
-            authorGif.attr("src", gifURL);
-            gifDiv.append(authorGif);
-
+            image.attr("src", imageURL);
+            image.attr("data-state", "still");
+            image.attr("data-still", response.data[i].images.fixed_height_small_still.url);
+            image.attr("data-animate", response.data[i].images.fixed_height_small.url);
+            gifFig.append(image);
+            gifFig.append(imageCaption);
+            gifDiv.append(gifFig);
         }
-
+        
+        
         gifRow.append(gifDiv);
         $("#gifVP").prepend(gifRow);
 
@@ -72,15 +78,18 @@ buttons ();
 
 $(document).on("click", ".author", displayGifs);
 
+$(document).on("click", ".gif", function() {
 
-
-
-
-
-
-
-
-
+    var state = $(this).attr("data-state");
+ 
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
 
 
 
